3a34eb43b68326a88e8ee67a8b9ca397ba38b6a9
Barata <samuel_barata@hotmail.com>
2023-03-21 13:45:48 +0000

    Struturing frontend

---
+++ frontend/src/components/App.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+return <div></div>;
---
+++ frontend/src/components/JobEntry/JobEntry.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function JobEntry() {
+  return (
+    <div>
+      <form>
+        <input></input>
+      </form>
+    </div>
+  );
+}
