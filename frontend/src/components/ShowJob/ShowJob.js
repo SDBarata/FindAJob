3df1a843208e4a003da78573362861027fc33ef1
Barata <samuel_barata@hotmail.com>
2023-03-24 14:03:24 +0000

    Getting data from backend

--- frontend/src/components/ShowJob/ShowJob.js
+++ frontend/src/components/ShowJob/ShowJob.js
@@ -2,27 +2,26 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 //import api from "../../services/api";
 
-function ShowJobs() {
-  const [jobs, setJobs] = useState();
+export default function ShowJobs() {
+  const [jobs, setJobs] = useState([]);
+
+  const baseURL = "http://localhost:3000/api/jobs";
 
   useEffect(() => {
-    axios.get("http://localhost:3000/api/jobs/").then((response) => {
+    const getJobs = async () => {
+      const response = await axios.get(baseURL);
       setJobs(response.data);
-    });
+      console.log("resposta", response);
+    };
+    getJobs();
   }, []);
-
-  function handleClick() {
-    return setJobs();
-  }
 
   return (
     <div>
       <h1>{jobs}</h1>
       <form>
-        <button onClick={handleClick}>Get available jobs!</button>
+        <button>Get available jobs!</button>
       </form>
     </div>
   );
 }
-
-export default ShowJobs;
