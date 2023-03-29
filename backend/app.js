6fba1dba168bf799f3dcbd3ca74a5111c6e8856b
Barata <samuel_barata@hotmail.com>
2023-03-29 05:09:14 +0100

    Add cors on the package

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const initDatabase = require("./src/db/database");
 
 initDatabase();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const router = require("./src/routes/routes");
 app.use("/api", router);
