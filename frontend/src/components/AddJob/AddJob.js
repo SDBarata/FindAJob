6924633b0a5fd8999ce001cf9ea1d4c5f4075c93
Barata <samuel_barata@hotmail.com>
2023-04-07 18:28:43 +0100

    Starting adding jobs

---
+++ frontend/src/components/AddJob/AddJob.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+export default function AddJob() {
+  [addNewJob, setNewJobs] = useState({
+    title: "",
+    description: "",
+  });
+
+  return <div></div>;
+}
