48843c429694598316d3f85722a6bce8f26e0895
Barata <samuel_barata@hotmail.com>
2023-06-24 03:22:18 +0100

    Styling Find a job page, alternatively improving the Add a job page adding a dropdown menu button, making some trials

--- frontend/src/pages/AddJob.js
+++ frontend/src/pages/AddJob.js
@@ -35,71 +35,125 @@ export default function AddJob() {
         <form className="addjob-input">
           <div className="input-block">
             <label for="title" className="label">
-              <p>Job title *</p>
-              <input
-                className="input"
-                onChange={handleChange}
-                type="text"
-                name="title"
-                placeholder="Add a title for the job"
-                value={newJob.title}
-              />
+              Job title *
             </label>
+            <input
+              className="input"
+              onChange={handleChange}
+              type="text"
+              name="title"
+              placeholder="Add a title for the job"
+              value={newJob.title}
+            />
           </div>
           <div className="input-block">
             <label for="company" className="label">
-              <p>Company *</p>
-              <input
-                className="input"
-                onChange={handleChange}
-                type="text"
-                name="company"
-                placeholder="Company's name"
-                value={newJob.company}
-              />
+              Company *
             </label>
+            <input
+              className="input"
+              onChange={handleChange}
+              type="text"
+              name="company"
+              placeholder="Company's name"
+              value={newJob.company}
+            />
           </div>
           <div className="input-block">
             <label for="location" className="label">
-              <p>Location *</p>
-              <input
-                className="input"
-                onChange={handleChange}
-                type="text"
-                name="location"
-                placeholder="Company's location"
-                value={newJob.location}
-              />
+              Location *
             </label>
+            <input
+              className="input"
+              onChange={handleChange}
+              type="text"
+              name="location"
+              placeholder="Company's location"
+              value={newJob.location}
+            />
           </div>
           <div className="input-block">
             <label for="description" className="label">
-              <p>Description *</p>
-              <input
-                className="input"
-                onChange={handleChange}
-                type="text"
-                name="description"
-                placeholder="Description"
-                value={newJob.description}
-              />
+              Description *
             </label>
+            <input
+              className="input"
+              type="text"
+              name="description"
+              placeholder="Description"
+              value={newJob.description}
+            />
           </div>
           <div className="input-block">
             <label for="jobtype" className="label">
-              <p>Job type *</p>
-              <input
-                className="input"
-                onChange={handleChange}
-                type="text"
-                name="jobtype"
-                placeholder="Add the jobtype"
-                value={newJob.jobtype}
-              />
+              Job type *
             </label>
+            <input
+              className="input"
+              onChange={handleChange}
+              type="text"
+              name="jobtype"
+              placeholder="Add the job type (e.g. Full/Part-time)"
+              value={newJob.jobtype}
+            />
             <p>* Mandatory fields</p>
           </div>
+
+          {/* <div className="jobtype-class">
+            <button
+              type="button"
+              className="button-jobtype"
+              aria-expanded="false"
+              aria-label="jobtype-menu"
+            ></button>
+            "Full time"
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              data-supported-dps="16x16"
+              fill="currentColor"
+              className="select-options"
+              width="16"
+              height="16"
+              focusable="false"
+            >
+              <path
+                d="M8 11L3 6h10z"
+                fill-rule="evenodd"
+                fill="currentColor"
+              ></path>
+            </svg>
+          </div>
+          <div className="" aria-hidden="true">
+            <ul>
+              <button value="fulltime" className="jobtype">
+                <div className="label">"Full-time"</div>
+              </button>
+            </ul>
+          </div> */}
+          <div className="input-block">
+            <label for="jobtype" className="label">
+              Job type *
+            </label>
+            <div className="jobtype-class">
+              <button type="button" className="jobtype-button button-menu1">
+                <span className="icons-class"></span>
+              </button>
+              <button type="button" className="jobtype-button button-menu2">
+                <span className="material-symbols-outlined">expand_more</span>
+              </button>
+              <div className="jobtype-list">
+                <button type="button" className="button-list">
+                  <span>"Full-time"</span>
+                </button>
+                <button type="button" className="button-list">
+                  <span>"Part-time"</span>
+                </button>
+              </div>
+            </div>
+          </div>
         </form>
+
         <div className="addjob-button">
           <button className="submit-button" type="submit" onClick={submitJob}>
             Add job
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -18,6 +18,256 @@ export default function Home() {
         ></div>
         <div className="page">
           <h1>Find the perfect job that fits you!!</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+            pretium sagittis erat, ut viverra nunc bibendum eget. Mauris ac
+            feugiat nulla, a pharetra ipsum. Morbi pellentesque, sapien quis
+            sollicitudin tincidunt, ante libero eleifend leo, ut lacinia turpis
+            velit id dui. Nam eget odio et odio interdum accumsan. Phasellus in
+            mi erat. Sed scelerisque, nulla et venenatis scelerisque, justo
+            velit iaculis mi, non aliquam tellus nibh vitae lacus. Aliquam sit
+            amet tempor dui. Aliquam pretium, urna id dignissim tempor, nisi
+            nulla condimentum nisi, id feugiat dui dolor a quam. Ut vitae
+            bibendum libero, id laoreet sapien. In ut neque luctus, venenatis
+            sem vitae, dictum leo. Duis sollicitudin dolor vitae lectus varius
+            vehicula. Mauris libero libero, maximus quis nisi a, tempus
+            condimentum orci. Sed eget laoreet arcu, sit amet tristique felis.
+            Sed et pellentesque tortor. Proin dignissim, dui eu elementum
+            scelerisque, ex elit pharetra nunc, vel egestas diam mi et ex. Donec
+            egestas dolor augue, eget hendrerit mi porttitor a. Nulla eu tortor
+            sollicitudin quam lacinia suscipit. Fusce at dapibus risus, vitae
+            mollis lorem. Nunc rhoncus quam et leo elementum, nec volutpat dolor
+            semper. Sed nec tincidunt nulla. Praesent eu lobortis risus. Fusce
+            dolor leo, suscipit ut libero vitae, sollicitudin dignissim tellus.
+            Suspendisse et tortor lacus. Phasellus pellentesque rutrum nisi.
+            Donec convallis elementum augue. Cras vitae erat rutrum, molestie
+            lectus quis, aliquet sapien. Nunc congue nisl eu malesuada dictum.
+            Suspendisse dapibus magna id enim mattis tempus. Integer placerat
+            massa vel arcu fermentum vehicula. Sed eu massa pulvinar, bibendum
+            eros elementum, lobortis tellus. Nam eget lacinia eros, id
+            condimentum magna. Pellentesque vitae odio varius, bibendum odio et,
+            ornare tellus. Nulla facilisi. Mauris molestie imperdiet neque
+            ultricies ultrices. Sed massa enim, blandit ut placerat ac,
+            tincidunt eget tortor. Duis scelerisque et ante eget aliquam.
+            Praesent tellus nisi, mattis ac urna venenatis, congue suscipit
+            risus. Donec bibendum arcu et orci tempor tincidunt. Etiam porttitor
+            urna et libero rutrum, sodales volutpat quam euismod. In porta, dui
+            ac dapibus maximus, leo orci scelerisque ante, sed pretium elit quam
+            id quam. Nulla tortor enim, porta id iaculis eget, euismod ac arcu.
+            Cras ornare eget lectus quis sollicitudin. Nunc in tortor quis nibh
+            rhoncus porttitor. Etiam condimentum dapibus tellus, quis laoreet
+            elit luctus sit amet. Aliquam a maximus nulla. Praesent molestie
+            ante ac nulla viverra bibendum. Praesent ut massa a nisl finibus
+            lobortis vel ac velit. Donec sollicitudin velit dolor, a ultrices
+            mauris volutpat et. In at libero ac orci ultricies semper finibus eu
+            odio. Praesent id lacus arcu. Morbi id arcu velit. Fusce ullamcorper
+            luctus vulputate. Etiam egestas, lectus vel laoreet placerat, quam
+            nisl vehicula mi, ut vestibulum erat magna eu tellus. Orci varius
+            natoque penatibus et magnis dis parturient montes, nascetur
+            ridiculus mus. Suspendisse potenti. Proin a risus tellus. Aenean
+            dapibus vel massa eu finibus. Proin ac turpis lobortis, rutrum erat
+            a, elementum nulla. Curabitur efficitur feugiat hendrerit. Maecenas
+            mollis, orci sed lobortis eleifend, ante odio fringilla lorem, in
+            pharetra nunc est et odio. Ut elementum, mi sed maximus cursus,
+            sapien velit aliquet est, fringilla accumsan eros turpis ut nunc.
+            Praesent malesuada commodo orci. Suspendisse auctor et quam sed
+            facilisis. Donec quam leo, suscipit a augue nec, porta tempus sem.
+            Praesent blandit mattis ipsum ut aliquet. Praesent eleifend sapien
+            et dolor egestas, et suscipit lacus suscipit. Morbi semper venenatis
+            erat, ut tincidunt tellus laoreet quis. Quisque et nulla ex.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+            eget mattis turpis, sit amet volutpat ante. Ut vulputate laoreet
+            augue, in mattis lorem venenatis a. Praesent eu finibus justo.
+            Phasellus bibendum elit sit amet justo bibendum porta eget a nisl.
+            Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris
+            tincidunt purus eu eros hendrerit dictum. Maecenas sodales pharetra
+            sapien. Integer mattis fringilla felis, vitae placerat mi consequat
+            eu. Phasellus eleifend fermentum arcu, ac sagittis urna fermentum
+            vitae. Nam venenatis suscipit libero, in rhoncus elit hendrerit in.
+            Quisque libero neque, rhoncus in erat ac, vehicula molestie mi. Cras
+            ullamcorper consequat arcu, non vulputate nibh dictum ut. Donec
+            ullamcorper orci diam, interdum feugiat felis hendrerit sed. Vivamus
+            sollicitudin leo non sapien tincidunt finibus. Suspendisse
+            pellentesque purus non odio blandit, id lacinia sem elementum.
+            Aenean congue fringilla euismod. Sed finibus sapien mauris, a
+            aliquet leo molestie eu. Pellentesque sit amet interdum nunc.
+            Suspendisse sodales sem nec faucibus interdum. Integer non pretium
+            orci, eget elementum nisl. Suspendisse cursus eros erat, eu sagittis
+            metus posuere sed. Cras viverra varius viverra. Nulla mi mauris,
+            consectetur sit amet tristique vel, ultricies sit amet tellus. Donec
+            neque turpis, aliquam pharetra dolor at, consequat dictum mi. Ut
+            sagittis diam nulla, vel ullamcorper diam vulputate sit amet. Cras
+            porta neque vitae lobortis elementum. Nam nisl eros, dapibus quis
+            consectetur a, tristique at nunc. Maecenas porta pellentesque purus
+            at maximus. In velit metus, vestibulum non turpis vitae, posuere
+            varius erat. Nam convallis, magna non vestibulum rhoncus, mauris
+            tellus maximus nisi, vitae faucibus tortor neque in elit. Sed
+            euismod lectus nisl, a rutrum ex pellentesque id. Nullam
+            sollicitudin, nulla eleifend condimentum tincidunt, tortor sem
+            finibus tellus, nec auctor ex massa in neque. Vestibulum ante urna,
+            posuere id interdum gravida, elementum sit amet purus. Aenean quis
+            risus venenatis, mollis justo vitae, fringilla odio. Pellentesque
+            hendrerit quis nulla mattis luctus. In hac habitasse platea
+            dictumst. Sed vitae elit egestas, molestie quam sit amet, bibendum
+            risus. Ut et interdum eros. Integer ac lacus purus. Nam libero erat,
+            placerat molestie ornare ac, pellentesque a tortor. Interdum et
+            malesuada fames ac ante ipsum primis in faucibus. Nulla eu luctus
+            erat. Praesent tempor eros at iaculis sodales. Fusce porttitor neque
+            et arcu tincidunt, id vehicula sem tincidunt. Proin ut purus
+            sagittis, euismod ligula vitae, congue est. Donec sit amet lectus
+            nisl. Integer est tortor, interdum id nunc eu, vehicula sagittis
+            lectus. Pellentesque felis eros, posuere ac tellus eu, efficitur
+            dapibus ex. Donec pretium, augue eu viverra hendrerit, erat nisi
+            hendrerit nulla, egestas cursus lorem dolor quis purus. Quisque sed
+            enim lacinia, varius diam in, ultrices augue. Nulla ligula erat,
+            dapibus eu dolor vel, tempor condimentum metus. Etiam at volutpat
+            odio. Nulla turpis nisi, dapibus eget scelerisque et, efficitur eget
+            arcu. Quisque vel laoreet massa, in gravida ligula. Donec fringilla
+            imperdiet lorem nec blandit. Aliquam tincidunt tortor non eros
+            efficitur, ac ullamcorper eros ornare. Mauris consequat vel purus
+            sed viverra. Fusce eleifend at enim vitae cursus. Cras et tempor
+            lectus, eget aliquam magna. Nulla vitae nibh nisi. Aenean gravida
+            tempor augue, in molestie turpis vestibulum at. Phasellus dapibus
+            laoreet magna, sit amet egestas nisl pharetra ut. Etiam tincidunt
+            dictum molestie. In fermentum aliquet aliquet. Nullam vitae turpis
+            at neque vestibulum pretium. Maecenas et ligula justo. Aenean
+            efficitur quam quis odio tincidunt porttitor. Integer nec quam
+            euismod, efficitur velit vel, dapibus mauris. Nam turpis nisi,
+            mollis at lectus vulputate, commodo cursus dui. Vestibulum at lectus
+            non felis varius cursus. Nullam sit amet tortor eu ex rhoncus
+            gravida eu et dui. In commodo non orci ac accumsan. Praesent in
+            metus vel quam pretium pharetra. Vivamus molestie, velit at laoreet
+            porttitor, ex augue facilisis arcu, nec scelerisque lectus diam
+            vitae erat. Nullam cursus sit amet justo maximus dapibus. Morbi
+            fringilla neque at tortor auctor ultrices. Suspendisse eu tortor
+            hendrerit, accumsan orci quis, tempus velit. Nullam tellus tellus,
+            pharetra et tempus in, imperdiet ut dolor. Donec non suscipit arcu.
+            Quisque turpis lacus, volutpat eu augue vitae, tincidunt varius
+            erat. Morbi ultricies nibh at turpis volutpat, ac rutrum tellus
+            ultrices. In convallis, orci sit amet feugiat ultricies, nisl urna
+            mollis mauris, id imperdiet felis purus id mauris. Donec a eleifend
+            ex, non pretium dui. Fusce feugiat massa in dolor eleifend, nec
+            volutpat turpis mattis. Phasellus vel felis nec risus blandit
+            aliquam in vel diam. Aliquam efficitur, magna at tempus iaculis,
+            erat neque semper augue, et dictum urna urna in nibh. Nam eget nibh
+            semper, ultrices est sed, tempor orci. Duis odio ligula, malesuada
+            eu vulputate vitae, molestie tincidunt turpis. Maecenas cursus
+            consectetur euismod. In consectetur turpis in mauris faucibus, quis
+            commodo neque ultricies. Aliquam bibendum metus in maximus
+            fermentum. In scelerisque elementum massa, vitae lobortis magna
+            mattis eget. Nulla vehicula erat nec cursus mollis. Nunc eu leo
+            scelerisque, laoreet metus at, semper elit. Pellentesque cursus
+            semper nisl ut pretium. Nulla facilisi. Sed rhoncus pellentesque
+            velit, faucibus feugiat diam lobortis quis. Nulla magna lectus,
+            molestie eget sem at, suscipit feugiat sapien. Nam mattis, nisl et
+            congue tristique, lorem arcu laoreet lectus, at tincidunt sem purus
+            vitae dui. Sed scelerisque venenatis massa ac porttitor. Proin quis
+            justo imperdiet, sodales dolor quis, ullamcorper diam. Praesent
+            fringilla in leo sit amet venenatis. Etiam a vehicula neque. Integer
+            ac eleifend urna, eget maximus leo. Donec faucibus arcu a orci
+            dapibus, ut dignissim metus ultricies. Quisque nec pharetra lacus.
+            Praesent id lacus ante. Mauris dictum blandit turpis. Praesent
+            aliquam lorem et mauris consequat congue. Etiam nec lobortis leo. In
+            hac habitasse platea dictumst. Curabitur quis tortor pellentesque,
+            fringilla metus ac, accumsan elit. Cras nisl nulla, tincidunt id ex
+            et, sodales facilisis dui. Aenean vestibulum neque ut ex
+            consectetur, a fermentum dolor semper. Curabitur faucibus, purus id
+            fringilla ornare, eros urna eleifend augue, in pharetra nunc quam at
+            eros. Cras augue tortor, efficitur at est in, porttitor consectetur
+            massa. Aliquam aliquam, erat vel porta auctor, ipsum libero
+            facilisis arcu, eget fringilla orci diam vitae odio. Sed eu
+            consectetur ligula. Praesent dignissim felis ut mollis dictum.
+            Curabitur ac ipsum pharetra, interdum diam ut, luctus orci.
+            Phasellus vitae tristique nisi. Etiam commodo tortor arcu, a
+            pharetra diam lobortis non. Phasellus vitae nibh a nisi fermentum
+            consectetur ac quis magna. Fusce ornare tempus ipsum, vel porta
+            tellus pulvinar sed. Morbi sollicitudin ligula id libero consectetur
+            imperdiet. Cras convallis placerat mollis. Donec rutrum id odio eget
+            imperdiet. Aenean egestas ornare mauris. Maecenas sit amet enim non
+            eros pretium rhoncus a sed nisi. Morbi viverra congue neque eget
+            tempus. Ut euismod vitae felis sit amet laoreet. Class aptent taciti
+            sociosqu ad litora torquent per conubia nostra, per inceptos
+            himenaeos. Nullam efficitur, nisi a convallis iaculis, purus nisl
+            posuere eros, eget commodo neque dolor ut augue. Fusce porta est
+            eget facilisis aliquet. Proin vehicula dolor et ultrices blandit.
+            Duis rhoncus lorem eget fringilla accumsan. Curabitur sollicitudin
+            diam ultrices, sodales mi a, fermentum urna. Aenean id vehicula
+            libero. Praesent quis pellentesque felis. In augue turpis, feugiat
+            et metus euismod, bibendum mattis augue. Phasellus porta blandit
+            arcu, ut tristique purus condimentum sit amet. Mauris consequat
+            congue est, sit amet mattis lorem pretium quis. Cras mollis mauris
+            vel ipsum vulputate, ac efficitur dui imperdiet. Sed finibus, ligula
+            non blandit consectetur, est turpis tempus nisl, sed accumsan ante
+            elit vitae velit. Sed tortor libero, tincidunt sit amet elit ut,
+            finibus auctor erat. Integer dapibus a elit non vestibulum. Fusce
+            fringilla, lacus in dignissim ornare, ex lorem ornare nibh, vel
+            sagittis nulla diam id nunc. In ultrices diam eu mauris commodo, sed
+            commodo quam gravida. Etiam porta ligula eget dictum blandit. Mauris
+            enim ligula, elementum sit amet sodales nec, suscipit vel justo. Ut
+            non sapien consequat, dignissim erat sit amet, faucibus orci.
+            Vestibulum fermentum turpis sed odio tincidunt, in ultrices augue
+            elementum. Morbi eu erat arcu. Morbi ornare laoreet tortor, ac
+            rhoncus libero cursus sed. Phasellus erat erat, vulputate non nunc
+            nec, dictum dapibus nibh. Interdum et malesuada fames ac ante ipsum
+            primis in faucibus. Quisque sit amet iaculis elit, eu ornare eros.
+            Morbi nunc nibh, vulputate porttitor turpis nec, faucibus aliquet
+            leo. Proin dignissim ipsum non lorem consequat tincidunt. Donec
+            elementum justo eget diam posuere fermentum. In semper, tortor sit
+            amet fringilla pulvinar, tellus eros aliquam urna, nec eleifend
+            ipsum felis sed neque. Vivamus porttitor sagittis dictum. Ut et
+            iaculis eros. Nam eleifend massa ut justo aliquam feugiat. Fusce
+            gravida eros semper nisi molestie, vel consectetur diam viverra.
+            Vestibulum sed lectus est. Fusce tempor auctor est eget tempus. Nunc
+            porttitor convallis augue, quis scelerisque mauris aliquet a.
+            Suspendisse varius mi sed venenatis tempor. Integer et nisi id arcu
+            blandit cursus sit amet vel eros. Nullam a congue tortor. Nam vitae
+            nisl ac velit rutrum mollis. Fusce vehicula condimentum dui eu
+            egestas. Pellentesque erat augue, hendrerit sit amet finibus eget,
+            faucibus sit amet nibh. Proin sit amet iaculis neque. Vivamus nec ex
+            non ipsum luctus condimentum. Praesent in nunc orci. Duis aliquam,
+            metus id semper placerat, nibh velit placerat odio, at dictum quam
+            turpis sit amet diam. In hac habitasse platea dictumst. Maecenas eu
+            dapibus libero, at rhoncus dolor. Pellentesque in dapibus ex.
+            Vivamus elementum pulvinar aliquet. Maecenas quis tristique ipsum.
+            In sagittis aliquam bibendum. Mauris sit amet ante finibus, laoreet
+            tortor ut, varius massa. Donec dictum ullamcorper orci vestibulum
+            gravida. Duis vel convallis sapien. Integer iaculis sem leo, ut
+            porttitor erat tempus et. Phasellus nisl nibh, consectetur non metus
+            sit amet, ultricies aliquam nulla. Duis quis ligula nec erat iaculis
+            vestibulum eget ac odio. Nam in arcu consectetur, dignissim nisl
+            eleifend, ultrices lorem. Suspendisse condimentum a nulla id
+            posuere. Sed eget varius magna. Nulla sit amet tristique lacus.
+            Nullam congue ut tellus sed sagittis. Class aptent taciti sociosqu
+            ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla
+            efficitur lacus viverra, gravida turpis nec, viverra lacus. Integer
+            accumsan magna ut ipsum vestibulum, imperdiet finibus dui tempor.
+            Aliquam suscipit lorem eget felis porta sodales. Pellentesque
+            finibus mi sit amet erat dignissim luctus. Nulla id mi vitae metus
+            vulputate molestie. Pellentesque at bibendum nisl. Duis pulvinar ac
+            justo sed bibendum. In hac habitasse platea dictumst. Donec
+            sollicitudin, sapien at interdum pharetra, leo sem sodales odio, ut
+            porta erat metus et enim. Morbi vitae lobortis urna. Suspendisse
+            potenti. Pellentesque commodo hendrerit neque vel condimentum. Sed
+            volutpat risus tincidunt urna pharetra volutpat. Nam at urna at sem
+            pulvinar consequat. Pellentesque ante nunc, semper vitae rutrum non,
+            aliquet aliquet est. Duis nec augue aliquam, hendrerit lacus non,
+            aliquam tortor. Suspendisse ultricies blandit eleifend. Fusce
+            pellentesque risus aliquam, consectetur urna eget, fermentum lectus.
+            Morbi ultrices ultricies orci, sed porta diam congue vitae. Morbi
+            faucibus tortor vel porttitor ornare. Quisque imperdiet nulla vitae
+            rhoncus sagittis. Donec placerat pellentesque efficitur. Mauris id
+            dolor vestibulum, vestibulum nulla in, convallis felis. Nulla
+            euismod, lacus sit amet rhoncus dignissim, est nisl rutrum odio, sit
+            amet egestas lectus velit vitae lorem. Nullam purus urna, cursus
+            consectetur pretium eu, molestie in neque. Sed sed condimentum
+            ligula, sit amet consequat sapien. Donec luctus mi eu orci euismod,
+            et euismod turpis pretium. Duis malesuada, nisl id mollis porta,
+            lacus arcu vestibulum lectus, sed tincidunt lorem nibh eu ante. Cras
+            laoreet vehicula odio, vitae feugiat libero.
+          </p>
         </div>
       </div>
     </>
